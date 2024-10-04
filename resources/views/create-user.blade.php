3c536281b7506fe0118282a6839e707ffe0652a5
Reffa Hibatullah <raffahibatullah888@gmail.com>
2024-10-04 20:15:19 +0700

    penggabungan staging

--- resources/views/create-user.blade.php
+++ resources/views/create-user.blade.php
@@ -4,7 +4,7 @@
     <div class="flex items-center justify-center h-screen">
         <div class="bg-blue-200 rounded-xl shadow-lg p-7">
             <h1 class="font-bold text-center pb-5">Create User</h1>
-            <form action="{{ route('user.store') }}" method="POST" class="flex flex-col items-center">
+            <form action="{{ route('user.store') }}" method="POST" enctype="multipart/form-data" class="flex flex-col items-center">
                 @csrf
                 <div class="flex flex-col">
                     <label for="nama" class="pb-1 font-bold">Nama:</label>
